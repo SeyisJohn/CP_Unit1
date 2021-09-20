c7543b46a7f35ee3269ff44314b101aa7d0a8165
Seyi Olajuyi <seyiolajuyi@Seyis-MacBook-Air.local>
2021-09-19 23:17:04 -0400

    uploading assignment2 through terminal

---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,15 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by Seyi Olajuyi on 9/19/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+    
+}
